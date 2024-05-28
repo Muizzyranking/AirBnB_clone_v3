2a2b2287d81885914030df78bdf16cd12fa6b7d5
Emmanuel <kcemmy03@gmail.com>
2024-05-28 12:39:46 +0100

    Created an Endpoint that retrieves the number of objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,3 +11,25 @@ def api_status():
         A JSON object containing the status of the API.
     """
     return {'status': 'OK'}
+
+@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
+def api_stats():
+    """Return the number of each object by type.
+
+    Returns:
+        A JSON object containing the number of each object by type.
+    """
+    from models.user import User
+    from models.state import State
+    from models.city import City
+    from models.amenity import Amenity
+    from models.place import Place
+    from models.review import Review
+    return {
+        'amenities': len(storage.all(Amenity).values()),
+        'cities': len(storage.all(City).values()),
+        'places': len(storage.all(Place).values()),
+        'reviews': len(storage.all(Review).values()),
+        'states': len(storage.all(State).values()),
+        'users': len(storage.all(User).values())
+    }
